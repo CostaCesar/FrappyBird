575c12c1df9e028582ac4af8b7e3b5fc3fb5414a
Caesar <2021951620@teiacoltec.org>
2023-05-31 14:37:15 -0300

    FIX: Remove debug remnants

--- Assets/Scripts/FadeIN.cs
+++ Assets/Scripts/FadeIN.cs
@@ -20,7 +20,6 @@ public class FadeIN : MonoBehaviour
         i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
         while (i.color.a < 1.0f)
         {
-            Debug.Log("Changing text");
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.unscaledDeltaTime / time));
             yield return null;
         }
@@ -30,7 +29,6 @@ public class FadeIN : MonoBehaviour
         i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
         while (i.color.a < 1.0f)
         {
-            Debug.Log("Changing image");
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.unscaledDeltaTime / time));
             yield return null;
         }
